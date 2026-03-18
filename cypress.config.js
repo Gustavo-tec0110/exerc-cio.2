533629a70285e851d61d13d94c7c2598826c0bbc
Gustavo <gustavo.sl.0110@gmail.com>
2026-03-18 03:41:10 -0300

    remove require do cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,7 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     baseUrl: 'https://ebac-agenda-contatos-tan.vercel.app',
     viewportWidth: 1280,
     viewportHeight: 720
   }
-})
+}
